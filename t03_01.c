6bbf6bbeceb8f33727ef89dd6b7d976a4b756304
ChyntiaTambunan <chyntiatambunan14@gmail.com>
2026-03-04 14:30:56 +0700

    Update t03_01.c

--- t03_01.c
+++ t03_01.c
@@ -1,36 +1,20 @@
 #include <stdio.h>
-#include <limits.h>
+#include <limits.h> // Untuk INT_MAX dan INT_MIN
 
 int main() {
     int n;
     int input_num;
     int min_val = INT_MAX;
     int max_val = INT_MIN;
-    int first_num = 0;
-    int second_num = 0;
-    int third_num = 0;
-    int fourth_num = 0;
 
     do {
         scanf("%d", &n);
     } while (n <= 0);
 
-    if (n < 4) {
-        return 1;
-    }
-
     for (int i = 0; i < n; i++) {
-        scanf("%d", &input_num);
-
-        if (i == 0) {
-            first_num = input_num;
-        } else if (i == 1) {
-            second_num = input_num;
-        } else if (i == 2) {
-            third_num = input_num;
-        } else if (i == 3) {
-            fourth_num = input_num;
-        }
+        do {
+            scanf("%d", &input_num);
+        } while (input_num < -100 || input_num > 100);
 
         if (input_num < min_val) {
             min_val = input_num;
@@ -42,6 +26,6 @@ int main() {
 
     printf("%d\n", min_val);
     printf("%d\n", max_val);
-  
+
     return 0;
 }
