97edecfefce4cd0b8d0c8801cb64b9602f07ccb0
ChyntiaTambunan <chyntiatambunan14@gmail.com>
2026-03-04 14:23:53 +0700

    chyntia
    yes

--- t03_02.c
+++ t03_02.c
@@ -8,11 +8,18 @@ int main() {
     int max_val = INT_MIN;
     int first_num = 0;
     int second_num = 0;
+    int third_num = 0;
+    int fourth_num = 0;
     double average_first_two = 0.0;
+    double average_third_fourth = 0.0;
 
     do {
         scanf("%d", &n);
     } while (n <= 0);
+
+    if (n < 4) {
+        return 1;
+    }
 
     for (int i = 0; i < n; i++) {
         scanf("%d", &input_num);
@@ -21,6 +28,10 @@ int main() {
             first_num = input_num;
         } else if (i == 1) {
             second_num = input_num;
+        } else if (i == 2) {
+            third_num = input_num;
+        } else if (i == 3) {
+            fourth_num = input_num;
         }
 
         if (input_num < min_val) {
@@ -31,13 +42,12 @@ int main() {
         }
     }
 
-    if (n >= 2) {
-        average_first_two = (double)(first_num + second_num) / 2.0;
-    }
+    average_first_two = (double)(first_num + second_num) / 2.0;
 
     printf("%d\n", min_val);
     printf("%d\n", max_val);
     printf("%.2f\n", average_first_two);
+    
 
     return 0;
 }
