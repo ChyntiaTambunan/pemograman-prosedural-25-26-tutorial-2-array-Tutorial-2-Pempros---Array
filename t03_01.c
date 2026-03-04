2db35fda3faf0a1ab4c646f8142ba9e798ff1bf2
ChyntiaTambunan <chyntiatambunan14@gmail.com>
2026-03-04 14:44:15 +0700

    chyn
    yes

--- t03_01.c
+++ t03_01.c
@@ -7,11 +7,13 @@ int main() {
     int min_val = INT_MAX;
     int max_val = INT_MIN;
 
+    // Meminta input n sampai n positif
     do {
         scanf("%d", &n);
     } while (n <= 0);
 
     for (int i = 0; i < n; i++) {
+        // Meminta input_num sampai input_num berada dalam rentang -100 hingga 100
         do {
             scanf("%d", &input_num);
         } while (input_num < -100 || input_num > 100);
